7a651f9d9815e4387950a9009c0914ff2e0a2fdc
Bernardo Soares <soaresbernardo1406@gmail.com>
2026-05-20 10:34:36 -0300

    feat: criar conexão com a API ViaCEP

---
+++ service/BuscaEndereco.java
@@ -0,0 +1,23 @@
+package br.com.alura.findcep.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class BuscaEndereco {
+    public String buscaEndereco(String cep) throws IOException, InterruptedException {
+        String enderecoApi = "https://www.viacep.com.br/ws/" + cep + "/json/";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(enderecoApi))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        return json;
+    }
+}
